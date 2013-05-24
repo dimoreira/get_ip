ec964e4a17c67d186338510c4e7807d56c286e0a
Diego Moreira <diegoalvesmoreira@gmail.com>
2013-05-24 04:16:58 -0300

    Fixed spec validation

--- spec/get_ip_spec.rb
+++ spec/get_ip_spec.rb
@@ -4,13 +4,13 @@ describe GetIp do
 
 	it "should return the remote ip address" do
 		get_ip = GetIp.remote
-		get_ip.should_not == false
+		get_ip.should_not be_nil
 	end
 
 	it "should return the local ip address" do
 		get_ip = GetIp.local
 		get_ip.should_not == 0
-		get_ip.should == "192.168.1.4"
+		get_ip.should_not be_nil
 	end
 
 end
